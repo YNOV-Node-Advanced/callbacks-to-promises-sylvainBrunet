5fd6cbf26d20704170ee113d27c17e12c5a5b321
sylvainBrunet <sylvain69740@gmail.com>
2018-05-31 09:37:00 +0200

    bla

--- callbacks.js
+++ callbacks.js
@@ -1,22 +1,26 @@
-function resizeImages(source, dest, width, height, callback) {
-    fs.readdir(source, (err, files) => {
-        if (err) {
-            callback(err);
-        } else {
-            files.forEach((filename, fileIndex) => {
-                gm(source + filename)
-                    .resize(width, height)
-                    .write(
-                        dest + "w" + width + "_" + filename,
-                        (err, values) => {
-                            if (err) {
-                                callback(err);
-                            } else if (fileIndex == files.length - 1) {
-                                callback();
+function resizeImages(source, dest, width, height) {
+    return new Promise(function (resolve, reject) {
+
+        fs.readdir(source, (err, files) => {
+            if (err) {
+                return reject(err);
+            } else {
+                files.forEach((filename, fileIndex) => {
+                    gm(source + filename)
+                        .resize(width, height)
+                        .write(
+                            dest + "w" + width + "_" + filename,
+                            (err, values) => {
+                                if (err) {
+                                    return reject(err);
+                                } else if (fileIndex == files.length - 1) {
+                                    resolve();
+                                }
                             }
-                        }
-                    );
-            });
-        }
-    });
+                        );
+                });
+            }
+        });
+    })
+
 }
